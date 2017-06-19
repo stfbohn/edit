da6239a27bfc3cfd61bbfc4c913e47f0154d9329
stfbohn <stfbohn@gmail.com>
2017-06-20 00:15:20 +0200

    white / before change

--- public/javascripts/font.js
+++ public/javascripts/font.js
@@ -4,15 +4,9 @@
 var xmlhttp = new XMLHttpRequest();
 var url = "https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyBQLd206nVJp5NKjsr7an_SrilLNSpXN5Q";
 var items = []; 
-var item_index = 5; 
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var myArr = JSON.parse(this.responseText);
-        items = shuffle(myArr.items);     
-    }
-};
-xmlhttp.open("GET", url, true);
-xmlhttp.send();
+var item_index = 0; 
+let f1 = document.getElementById('f1'); 
+let f2 = document.getElementById('f2'); 
 
 function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
@@ -32,6 +26,17 @@ function shuffle(array) {
   return array;
 }
 
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myArr = JSON.parse(this.responseText);
+        items = shuffle(myArr.items);
+        clickdiv(f1);
+        clickdiv(f2);
+    }
+};
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+
 var loadFont = function(font) {
   WebFont.load({
     google: {
@@ -40,42 +45,79 @@ var loadFont = function(font) {
   });
 };
 
+f1.addEventListener("click", function() {
+        clickdiv(f2);
+    }); 
+f2.addEventListener("click", function() {
+        clickdiv(f1);
+    }); 
 
-/*
-AIzaSyBQLd206nVJp5NKjsr7an_SrilLNSpXN5Q
-*/
+let wbh = document.getElementById('wbh'); 
+let wb = document.getElementById('wb'); 
+let wh = document.getElementById('wh'); 
+
+let change_header = true;
+let change_body = true; 
+
+function what(x)
+{
+    if(x=='bodyheader') {
+
+        wbh.classList.add('sel'); 
+        wb.classList.remove('sel'); 
+        wh.classList.remove('sel'); 
+        change_header = true;
+        change_body = true; 
+
+    }
+    else if(x=='body') {
+        wbh.classList.remove('sel'); 
+        wb.classList.add('sel'); 
+        wh.classList.remove('sel'); 
+        change_header = false;
+        change_body = true; 
+    }
+    else if(x == 'header') {
+        
+        wbh.classList.remove('sel'); 
+        wb.classList.remove('sel'); 
+        wh.classList.add('sel');
+        change_header = true;
+        change_body = false;  
+    }
+}
+
+function colors(openOrClose) {
+    f2.getElementsByClassName('fontdiv')[0].classList.toggle('hidden'); 
+    f2.getElementsByClassName('blabla')[0].classList.toggle('hidden'); 
+}
 
 function clickdiv(parent) {
-
     let d = parent.getElementsByClassName('fontdiv')[0];
-
-
     item_index++;
     if(item_index >= items.length) {
         item_index = 0; 
     } 
-    d.classList.toggle('clicked');
-    setTimeout(function(){
-        var font = items[item_index].family;
-        loadFont(font);
-        d.style.fontFamily = font;
-
-        let fontname = d.parentElement.parentElement.getElementsByClassName('fontinfo')[0].getElementsByClassName('name')[0]; 
-        fontname.innerText = font;  
-        fontname.style.fontFamily = font;
-
-        //var h = d.getElementsByClassName('fontname'); 
-        //h[0].innerText = font; 
-        d.classList.toggle('clicked');
-    }, 300);
-}
-
-var divs = document.getElementsByClassName('fontcont'); 
-for(let i = 0; i <divs.length; i++) {
-    let x = divs.item(i);
-    divs.item(i).addEventListener("click", function() {
-
-        clickdiv(x);
-    } /*clickdiv(divs.item(i)*/);  
-    //console.log(divs.item(i).innerText); 
+    var font = items[item_index].family;
+    loadFont(font);
+    if(change_header) {
+        console.log('header');
+        let hhh = d.getElementsByClassName('hhh'); 
+        for(let i=0;i<hhh.length;i++) {
+            hhh[i].style.fontFamily = font; 
+        }
+        let hname = d.parentElement.parentElement.getElementsByClassName('hname')[0]; 
+        hname.innerText = font;  
+        hname.style.fontFamily = font;
+    }
+    if(change_body) {
+        console.log('body');
+        let bbb = d.getElementsByClassName('bbb'); 
+        for(let i=0;i<bbb.length;i++) {
+            bbb[i].style.fontFamily = font; 
+        }
+        let bname = d.parentElement.parentElement.getElementsByClassName('bname')[0]; 
+        bname.innerText = font;  
+        bname.style.fontFamily = font;
+    }
 }
